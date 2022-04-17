5e63a3117ba3a5beeeb85fd606d1f5f8ec3de63d
Mohamed Elgohary <87489620+MohamedElgohary88@users.noreply.github.com>
2022-04-17 14:26:04 +0200

    login logic + move from LoginActivity.kt to MainActivity.kt

--- app/src/main/java/com/example/mystory/LoginActivity.kt
+++ app/src/main/java/com/example/mystory/LoginActivity.kt
@@ -1,12 +1,57 @@
 package com.example.mystory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        login()
+
     }
+
+    private fun login() {
+
+        val arr:ArrayList<User> = ArrayList()
+        arr.add(User("mm","33"))
+        arr.add(User("aa","11"))
+        arr.add(User("bb","22"))
+
+        login.setOnClickListener {
+            val username = user_name?.text.toString()
+            val password = password?.text.toString()
+
+            val user = User(username,password)
+
+            for (userArray in arr) {
+                if ( userArray.email == user.email &&
+                    userArray.password == user.password ){
+                    Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show()
+                    break
+                }else{
+                    Toast.makeText(this,"Please Check Your Data",Toast.LENGTH_SHORT).show()
+                }
+            }
+            var i = Intent(this,MainActivity::class.java)
+            i.putExtra("email",user.email)
+            startActivity(i)
+        }
+    }
+
+    private fun checkFields() {
+  login.setOnClickListener { if(user_name?.text?.isEmpty() == true){
+      user_name.error = "Please Check Your User Name"
+  }else if ( password?.text?.isEmpty() == true) {
+      user_name.error = "Please Check Your User Name"
+  }else if (checkbox?.isChecked != true){
+      user_name.error = "Please Check Your User Name"
+  } else{
+      Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show() } }
+    }
+
 }
---
+++ app/src/main/java/com/example/mystory/User.kt
@@ -0,0 +1,3 @@
+package com.example.mystory
+
+class User (var email:String , var password:String )
