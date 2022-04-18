46370d789eed88f0a1c3e657f8bb130f31dae931
Mohamed Elgohary <87489620+MohamedElgohary88@users.noreply.github.com>
2022-04-18 09:19:41 +0200

    Create Fun DrawerClicks

--- app/src/main/java/com/example/mystory/MainActivity.kt
+++ app/src/main/java/com/example/mystory/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.mystory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,6 +17,59 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val email = intent.getStringExtra("email")
-        textview?.text = email
+        textview?.text =email
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        drawerClicks()
+        setUpDrawer()
+        updateEmailInHeader(email!!)
+
+    }
+
+    private fun drawerClicks() {
+        navigation_view?.setNavigationItemSelectedListener {
+            when (it.itemId){
+               R.id.home -> {
+                   drawer_layout?.closeDrawer(GravityCompat.START)
+               true
+               }
+               R.id.log_out -> {
+                   finish()
+                   val i = Intent(this,LoginActivity::class.java)
+                   startActivity(i)
+
+               true
+               }
+                else-> true
+            }
+        }
+    }
+
+    private fun updateEmailInHeader(email:String) {
+        val headerView = navigation_view?.getHeaderView(0)
+        val textViewEmail = headerView?.findViewById<TextView>(R.id.email_header)
+        textViewEmail?.text = email
+    }
+
+    private fun setUpDrawer() {
+        val toggle = ActionBarDrawerToggle(this,drawer_layout
+            ,R.string.open,R.string.close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home -> {
+                drawer_layout?.openDrawer(GravityCompat.START)
+                true
+
+            }
+            else -> true
+        }
+
     }
 }
